3a0213c746d74a1d268920b114c8332d8ce53a94
Mykyta <dsw49351@student.dsw.edu.pl>
2023-06-09 16:43:32 +0200

    task2

--- zadanie.py
+++ zadanie.py
@@ -1,7 +1,16 @@
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Opis programu')
 parser.add_argument('-a', '--argument', help='Opis argumentu')
 args = parser.parse_args()
 if args.argument:
     print(f'Przekazany argument: {args.argument}')
+def load_json_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+            return data
+    except Exception as e:
+        print(f'Błąd podczas wczytywania pliku JSON: {e}')
+        return None
